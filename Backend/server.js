b9bc2f6386e85decacd2bb59878226aa3fd0bb2a
Shashank Singh <sshingh456@gmail.com>
2026-08-25 13:54:24 +0530

    Refactor server.js and add Vercel configuration file

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,12 @@
-require("dotenv").config()
-const app = require("./src/app")
+// require("dotenv").config()
+// const app = require("./src/app")
 
-app.listen(3000, () => {
-    console.log("server is running on port 3000");
-})       
+// app.listen(3000, () => {
+//     console.log("server is running on port 3000");
+// })       
+
+require("dotenv").config();
+
+const app = require("./src/app");
+
+module.exports = app;
